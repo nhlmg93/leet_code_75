8bc33109ff2b6d554d7965256aea97053f1c5a47
Nathan Helmig <nhelmig93@gmail.com>
2022-05-08 08:15:47 -0500

    grouped anagrams

---
+++ arrays_hashing/group_anagrams.js
@@ -0,0 +1,17 @@
+function group_anagrams(anagrams, count = [], hash = {}){
+    for(const word of anagrams){
+        count = Array(26).fill(0)
+        for(const letter of word){
+            // a = 80 -> 0, 80 - 80
+            // b = 81 -> 1, 81 - 80
+            // ...
+            // z = 106 -> 25, 105 - 80
+            count[letter.charCodeAt(0) - 'a'.charCodeAt(0)] += 1
+        }
+        hash[count] ? hash[count].push(word) : hash[count] = [word]
+    }
+    return Object.keys(hash).map(key => hash[key])
+}
+
+
+console.log(group_anagrams(['eat', 'tea', 'tan', 'ate', 'nat', 'bat']))
