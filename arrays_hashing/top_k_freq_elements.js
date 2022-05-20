b48889b6a4a1e9ac0667d9eaa5d9890dacb1a254
Nathan Helmig <nhelmig93@gmail.com>
2022-05-20 15:07:05 -0500

    clean up

--- arrays_hashing/top_k_freq_elements.js
+++ arrays_hashing/top_k_freq_elements.js
@@ -1,23 +1,12 @@
-function top_k_req_elements(nums, k, count = {}){
-    let freq = new Array(nums.length)
-    for (let i = 0; i < nums.length; i++) {
-        freq[i] = [] 
-    }
-    
-
-    for (let i = 0; i < nums.length; i++) {
-        const element = nums[i];
-        count[element] ? count[element] += 1 : count[element] = 1    
-    }
+function top_k_req_elements(nums, k, count = {}, freq=[], res =[]){
+    nums.forEach(element => {
+        freq.push([])
+        count[element] ? count[element] += 1 : count[element] = 1
+    });
     
     for(const [key, value] of Object.entries(count)){
-        console.log(key,value)
         freq[value].push(key)
-        console.log(freq)
     }
-
-    console.log(freq)
-    let res = []
     for (let i = freq.length - 1; i > 0; i--) {
         for (const n of freq[i]){
             res.push(n)
