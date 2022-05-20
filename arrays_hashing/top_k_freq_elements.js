d27c6df4124a0e19cd013287030552eb069cbf0c
Nathan Helmig <nhelmig93@gmail.com>
2022-05-20 14:44:53 -0500

    lol javascript and creating pointers under the hood for you

--- arrays_hashing/top_k_freq_elements.js
+++ arrays_hashing/top_k_freq_elements.js
@@ -1,16 +1,22 @@
-function top_k_req_elements(nums, k, count = {}, freq = []){
-    for (let i = 0; i < nums.length + 1; i++) {
-        freq.push([])
+function top_k_req_elements(nums, k, count = {}){
+    let freq = new Array(nums.length)
+    for (let i = 0; i < nums.length; i++) {
+        freq[i] = [] 
     }
     
-    nums.forEach(element => {
-        count[element] ? count[element] += 1 : count[element] = 1
-    });
+
+    for (let i = 0; i < nums.length; i++) {
+        const element = nums[i];
+        count[element] ? count[element] += 1 : count[element] = 1    
+    }
     
     for(const [key, value] of Object.entries(count)){
+        console.log(key,value)
         freq[value].push(key)
+        console.log(freq)
     }
 
+    console.log(freq)
     let res = []
     for (let i = freq.length - 1; i > 0; i--) {
         for (const n of freq[i]){
@@ -22,4 +28,4 @@ function top_k_req_elements(nums, k, count = {}, freq = []){
     }
 }
 
-console.log(top_k_req_elements([1,1,1,2,2,3], 2))
+console.log(top_k_req_elements([1,1,1,2,2,2,1,1], 1))
