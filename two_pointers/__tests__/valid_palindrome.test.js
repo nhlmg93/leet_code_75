404934f294381e66d5a0a29539f1925f3fa263a0
Nathan Helmig <nhelmig93@gmail.com>
2022-05-24 15:58:05 -0500

    getting rid of some random stuff

--- two_pointers/__tests__/valid_palindrome.test.js
+++ two_pointers/__tests__/valid_palindrome.test.js
@@ -5,4 +5,10 @@ test("valid palindrome", () => {
     expect(
         valid_palindrom("A man, a plan, a canal: Panama")
     ).toBeTruthy()
+    expect(
+        valid_palindrom("")
+    ).toBeTruthy()
+    expect(
+        valid_palindrom("0P")
+    ).toBeFalsy()
 })
--- two_pointers/valid_palindrom.js
+++ two_pointers/valid_palindrom.js
@@ -1,28 +1,33 @@
 function valid_palindrom(palindrome) {
   let p1 = 0;
   let p2 = palindrome.length - 1;
-  const lowerBound = "a".charCodeAt(0);
-  const upperBound = "z".charCodeAt(0);
   while (p1 < p2) {
-      let a = palindrome.charAt(p1).toLowerCase()
+    let a = palindrome.charAt(p1).toLowerCase();
     while (
-      a.charCodeAt(0) < lowerBound ||
-      a.charCodeAt(0) > upperBound ||
-      a.charAt(0) === ' '
+      (a.charCodeAt(0) < "0".charCodeAt(0) ||
+        a.charCodeAt(0) > "9".charCodeAt(0)) &&
+      (a.charCodeAt(0) < "a".charCodeAt(0) ||
+        a.charCodeAt(0) > "z".charCodeAt(0) ||
+        a.charAt(0) === " ")
     ) {
       p1++;
-      a =  palindrome.charAt(p1).toLowerCase()
+      a = palindrome.charAt(p1).toLowerCase();
     }
-    let b = palindrome.charAt(p2).toLowerCase()
+    let b = palindrome.charAt(p2).toLowerCase();
     while (
-      b.charCodeAt(0) < lowerBound ||
-      b.charCodeAt(0) > upperBound ||
-      b.charAt(0) === ' '
+      ((b.charCodeAt(0) < "0".charCodeAt(0) ||
+        b.charCodeAt(0) > "9".charCodeAt(0)) &&
+        b.charCodeAt(0) < "a".charCodeAt(0)) ||
+      b.charCodeAt(0) > "z".charCodeAt(0) ||
+      b.charAt(0) === " "
     ) {
       p2--;
-      b = palindrome.charAt(p2).toLowerCase()
+      b = palindrome.charAt(p2).toLowerCase();
     }
-    if (palindrome.charAt(p1).toLowerCase() !== palindrome.charAt(p2).toLowerCase()) {
+    if (
+      palindrome.charAt(p1).toLowerCase() !==
+      palindrome.charAt(p2).toLowerCase()
+    ) {
       return false;
     } else {
       p1++;
@@ -33,5 +38,5 @@ function valid_palindrom(palindrome) {
   return true;
 }
 
-console.log(valid_palindrom("A man, a plan, a canal: Panama"));
+console.log(valid_palindrom("0P"));
 module.exports = valid_palindrom;
