ea05f35faac2b137bd7449c4d30357151b5ad03a
Nathan Helmig <nhelmig93@gmail.com>
2022-06-21 08:32:32 -0500

    description for encode decode

--- arrays_hashing/encode_decode_strings.js
+++ arrays_hashing/encode_decode_strings.js
@@ -1,5 +1,5 @@
 function encode(words){
-    return words.map(word => new String(word.length) + '#' + word).join('');
+    return words.map(word => String(word.length) + '#' + word).join('');
 }
 function decode(str) {
     let words = []
@@ -19,3 +19,9 @@ function decode(str) {
 const encodedString = encode(["Apple", "Tree"])
 console.log(encodedString)
 console.log(decode(encodedString))
+
+//encode scheme: [Apple, Tree] --> 5#Apple4#Tree
+//decode: while idx is not the length of the array...
+//let j = idx and if the char at j is the # then 
+//the num to the left is the end of the slice and
+//j is the beginning. parse slice into array.
