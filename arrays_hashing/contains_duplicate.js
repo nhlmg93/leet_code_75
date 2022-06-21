fdff208a95035ea04ed2bc953d26a991c1ec0a50
Nathan Helmig <nhelmig93@gmail.com>
2022-06-20 20:12:12 -0500

    added comment description

--- arrays_hashing/contains_duplicate.js
+++ arrays_hashing/contains_duplicate.js
@@ -10,3 +10,7 @@ function contains_duplicate(nums, hashset) {
 
 console.log(contains_duplicate([2,3,4,9,10,3,8,1], new Set()))
 console.log(contains_duplicate([1,2,3,4], new Set()))
+
+//iterate through the nums adding them to the set. 
+//check if the num is in the set. If the num  is in the set we have a duplicate
+//else there is no duplicate
