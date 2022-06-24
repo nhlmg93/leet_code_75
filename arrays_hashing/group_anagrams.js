509e023fb221173a5722d64c60f84cbf9846607c
nhlmg93 <62121934+nhlmg93@users.noreply.github.com>
2022-06-23 21:00:05 -0500

    added a description "hint" to group anagrams

--- arrays_hashing/group_anagrams.js
+++ arrays_hashing/group_anagrams.js
@@ -1,4 +1,6 @@
-function group_anagrams(anagrams, count = [], hash = {}){
+function group_anagrams(anagrams){
+    let count = []
+    let hash = {}
     for(const word of anagrams){
         count = Array(26).fill(0)
         for(const letter of word){
@@ -16,3 +18,10 @@ function group_anagrams(anagrams, count = [], hash = {}){
 //O(m * n)
 
 console.log(group_anagrams(['eat', 'tea', 'tan', 'ate', 'nat', 'bat']))
+//         A  B  C  D  E     T
+//count = [1][ ][ ][ ][1]...[1]
+//hash = key:count, value:[eat]
+//we store idx alphabetical representation of the word in the count array 
+//this count array then becomes the key to the bucket for the word to be input too
+//or the creation of said bucket
+//this is important because we know anagrams have the same letters
